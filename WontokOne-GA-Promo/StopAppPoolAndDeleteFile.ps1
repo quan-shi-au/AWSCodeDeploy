b2aa87c5355eeb3ab91b33ecba9e0b6c95461875
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2018-08-17 08:55:08 +1000

    no message

--- WontokOne-GA-Promo/StopAppPoolAndDeleteFile.ps1
+++ WontokOne-GA-Promo/StopAppPoolAndDeleteFile.ps1
@@ -15,7 +15,7 @@ if ($PSHOME -like "*SysWOW64*")
 
 Import-Module WebAdministration
 
-$TargetPath="c:\inetpub\wwwroot\WontokOne_GA_API"
+$TargetPath = "c:\inetpub\wwwroot\WontokOne_GA_Promo"
 $uniqueFolder=[guid]::NewGuid()
 $RootBackupPath="c:\Temp\Backup"
 $BackupPath="$RootBackupPath\$uniqueFolder"
