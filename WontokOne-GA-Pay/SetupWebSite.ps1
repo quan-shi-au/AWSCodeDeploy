c507d78c06e9324a4d4d80d0c226e230c2b062c0
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2018-08-16 14:16:23 +1000

    no message

--- WontokOne-GA-Pay/SetupWebSite.ps1
+++ WontokOne-GA-Pay/SetupWebSite.ps1
@@ -22,6 +22,7 @@ $GAPayAppPoolName = "WontokAppPool"
 $iisAppPoolDotNetVersion = "v4.0"
 $GAPayWebSiteName = "WontokOne GA PAY"
 $iisHostName = "ga-pay.wontokone.com"
+$certificateDnsName = '*.wontokone.com'
 $GAPayWebFilePath = "c:\inetpub\wwwroot\WontokOne_GA_PAY"
 $GaApiApplicationName = "api"
 $GaApiApplicationPath = "c:\inetpub\wwwroot\WontokOne_GA_PAY\api"
@@ -32,34 +33,34 @@ $ReconWebFilePath = "c:\inetpub\wwwroot\recon"
 $ReconIndexFile = "c:\inetpub\wwwroot\recon\index.html"
 $ReconAppPoolName = "ReconAppPool"
 
+#check if the site exists
+if (Test-Path $GAPayWebSiteName -pathType container)
+{
+    Write-Output "$GAPayWebSiteName Sites already exist. exit."
+    return
+} else {
+    Write-Output "Create Web Site $GAPayWebSiteName"
+}
+
 # Create folder and files
-New-Item -ItemType Directory -Force -Path $GAPayWebFilePath
 New-Item -ItemType Directory -Force -Path $ReconWebFilePath
 New-Item -ItemType File -Force -Path $ReconIndexFile
 
 #navigate to the app pools root
 Set-Location IIS:\AppPools\
 
-#remove default Website if exists
-Get-WebSite -Name "Default Web Site" | Remove-WebSite -Confirm:$false -Verbose
-Get-WebSite -Name "Default Website" | Remove-WebSite -Confirm:$false -Verbose
-Get-WebSite -Name "recon" | Remove-WebSite -Confirm:$false -Verbose
-
-#check if the app pool exists
+#check if the GA-Pay app pool exists
 if (!(Test-Path $GAPayAppPoolName -pathType container))
 {
     Write-Output "Create AppPool $GAPayAppPoolName"
-     #create the app pool
-     $appPool = New-Item $GAPayAppPoolName
-     $appPool | Set-ItemProperty -Name "managedRuntimeVersion" -Value $iisAppPoolDotNetVersion
+    $appPool = New-Item $GAPayAppPoolName
+    $appPool | Set-ItemProperty -Name "managedRuntimeVersion" -Value $iisAppPoolDotNetVersion
+    Set-ItemProperty IIS:\AppPools\WontokAppPool -name processModel -value @{userName="WontokAppPool";password="w0nt0k@123";identitytype=3}
 } else {
     Write-Output "AppPool $GAPayAppPoolName already exists."
 }
 
-#Assign user to AppPool
-Set-ItemProperty IIS:\AppPools\WontokAppPool -name processModel -value @{userName="WontokAppPool";password="w0nt0k@123";identitytype=3}
-
-#create default website's app pool
+#create Recon website's app pool
 if (!(Test-Path $ReconAppPoolName -pathType container))
 {
      #create the app pool
@@ -70,45 +71,43 @@ if (!(Test-Path $ReconAppPoolName -pathType container))
 #navigate to the sites root
 Set-Location IIS:\Sites\
 
-#check if the site exists
-if (Test-Path $GAPayWebSiteName -pathType container)
-{
-	#restart website
-    Write-Output "restart $GaApiApplicationName"
-	Stop-WebSite $GaApiApplicationName
-	Start-WebSite $GaApiApplicationName
-	
-    Write-Output "restart $GAPayWebSiteName"
-	Stop-WebSite $GAPayWebSiteName 
-	Start-WebSite $GAPayWebSiteName
-	
-    Write-Output "Sites already exist. exit."
-	
-    return
-} else {
-    Write-Output "Create Web Site $GAPayWebSiteName"
-}
-
-#create the site
-#$iisApp = New-Item $GAPayWebSiteName -bindings @{protocol="http";bindingInformation=":80:" + $iisHostName} -physicalPath $GAPayWebFilePath
+#create GA-Pay site
 $iisApp = New-Item $GAPayWebSiteName -bindings @{protocol="http";bindingInformation=":80:" + $iisHostName} -physicalPath $GAPayWebFilePath
 $iisApp | Set-ItemProperty -Name "applicationPool" -Value $GAPayAppPoolName
 
-#create default site
+#create Recon site
 $iisDefaultApp = New-Item $ReconAppName -bindings @{protocol="http";bindingInformation=":80:"} -physicalPath $ReconWebFilePath
 $iisDefaultApp | Set-ItemProperty -Name "applicationPool" -Value $ReconAppPoolName
 
 #assign ports 433
-New-WebBinding -Name $GAPayWebSiteName -IP "*" -Port 443 -Protocol https -HostHeader $iisHostName
+$MyCert = Get-ChildItem cert:\LocalMachine\MY -DnsName $certificateDnsName
 
-#assign certificate
-#Set-Location IIS:\SslBindings
-#Get-ChildItem cert:\LocalMachine\MY | Where-Object {$_.Subject -match "CN=VM*"} | Select-Object -First 1 | New-Item 0.0.0.0!443 
+if ($MyCert)
+{
+    Write-Host "$MyCert"
+}else {
+    Write-Host "Certificate doesn't exist - DNS Name $certificateDnsName"
+}
+
+# get the web binding of the site
+$binding = Get-WebBinding -Name $siteName -Protocol "https"
+
+if(!$binding)
+{
+    Write-Host "Create SSL binding"
+
+    New-WebBinding -Name $siteName -IP "*" -Port 443 -Protocol https -HostHeader $hostName
+    $binding = Get-WebBinding -Name $siteName -Protocol "https"
+
+    # set the ssl certificate
+     $binding.AddSslCertificate($MyCert.GetCertHashString(), "my")
+    
+} else {
+    Write-Host "SSL Binding already exists"
+}
 
 #create API application
 New-WebApplication $GaApiApplicationName -Site $GAPayWebSiteName -ApplicationPool $GAPayAppPoolName -PhysicalPath $GaApiApplicationPath
-
-
 
 #iisreset
 invoke-command -scriptblock {iisreset}
